d77d6de3c1a409ad14c91569893062239f246979
mwaura jonathan <mwaurajonathan019@gmail.com>
2023-12-13 16:35:42 +0300

    added array.js, bp.html, callback.css

--- array.js
+++ array.js
@@ -1,15 +1,29 @@
+const form = document.getElementById('order-form');
+const confirmation = document.getElementById('confirmation');
 
-  function concatAll(strings) {
-    strings.reduce((array, currentString) => {
-       stringArray=[ "one","two","three"]
-      array += currentString;
-      return array;
-    });
-    return result;
-  }
-  const strings = ["one", "two", "three"];
-  const concatenatedString = concatAll(strings);
-  console.log(concatenatedString);
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const item = document.getElementById('item').value;
 
 
-
+  placeOrder(name, item, function(response) {
+    if (response.status === 'success') {
+      confirmation.innerText = `Order confirmed for ${name}. You ordered ${item}`;
+    } else {
+      confirmation.innerText = `Order failed : ${response.error}`;
+    }
+  });
+});
+function placeOrder(name, item, callback) {
+  
+  setTimeout(function() {
+    const responses = [
+      { status: 'success' },
+      { status: 'error', error: 'Item unavailable' },
+    ];
+    const response = responses[Math.floor(Math.random() * responses.length)];
+    callback(response);
+  }, 2000);
+}
